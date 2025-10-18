569fb5fdbd2122102bb15e3fdfc007af0435909d
GHOSTXWAR <meintjesdirk6@gmail.com>
2025-10-18 16:30:28 +0200

    1

--- Assets/_project/Network/scripts/PlayerNetwork.cs
+++ Assets/_project/Network/scripts/PlayerNetwork.cs
@@ -12,47 +12,48 @@ public class PlayerNetwork :  NetworkBehaviour
     //virtual- overide google
 
 
-    private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(new MyCustomData { _int = 56, _bool = true,} , NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+ //   private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(new MyCustomData { _int = 56, _bool = true,} , NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    /*
 
+  public struct MyCustomData : INetworkSerializable
+  {
+      public int _int;
+      public bool _bool;
+      public FixedString128Bytes message;
+      public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+      {
+          serializer.SerializeValue(ref _int);
+          serializer.SerializeValue(ref _bool);
+          serializer.SerializeValue(ref message);
+      }
+  }
 
-    public struct MyCustomData : INetworkSerializable
-    {
-        public int _int;
-        public bool _bool;
-        public FixedString128Bytes message;
-        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
-        {
-            serializer.SerializeValue(ref _int);
-            serializer.SerializeValue(ref _bool);
-            serializer.SerializeValue(ref message);
-        }
-    }
-
-    public override void OnNetworkSpawn()
-    {
-        randomNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) =>
-        {
-            Debug.Log(OwnerClientId + "; " + newValue._int + "; " + newValue._bool + "; " + newValue.message);
-        };
-    }
-
+  
+  public override void OnNetworkSpawn()
+  {
+      randomNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) =>
+      {
+          Debug.Log(OwnerClientId + "; " + newValue._int + "; " + newValue._bool + "; " + newValue.message);
+      };
+  }
+  */
     private void Update()
     {
        
 
         if (!IsOwner) return;
 
-        if (Input.GetKeyDown(KeyCode.T))
+       // if (Input.GetKeyDown(KeyCode.T))
         {
         //    TestServerRpc();
 
             
-            randomNumber.Value = new MyCustomData
-            {
-                _int = 10,
-                _bool = false,
-                message = "All your gold is mine!"
-            };
+         //   randomNumber.Value = new MyCustomData
+         //   {
+            //    _int = 10,
+          //      _bool = false,
+           //     message = "All your gold is mine!"
+        //    };
             
         }
 
@@ -66,6 +67,10 @@ public class PlayerNetwork :  NetworkBehaviour
 
         float moveSpeed = 3f;
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+
+
+
     }
     /*
     [ServerRpc]
